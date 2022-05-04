ef73b271c5b603bd733d11e95f2855d798a549c2
lucassouzapb <lycasoliveira@gmail.com>
2022-05-04 16:38:34 -0300

    Utilizar api #6

--- lib/views/time/time.dart
+++ lib/views/time/time.dart
@@ -41,7 +41,7 @@ class _TimeViewState extends State<TimeView> {
               tabs: <Widget>[
                 Tab(
                   icon: Icon(Icons.stacked_bar_chart),
-                  text: "Estatísticas",
+                  text: "Estatística",
                 ),
                 Tab(
                   icon: Icon(Icons.emoji_events),
